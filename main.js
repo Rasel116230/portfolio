464bc9586500954584745abf57eb9f65d5c3d50e
Rasel673 <raselamin673@gmail.com>
2025-03-03 15:30:52 +0600

    changes

--- main.js
+++ main.js
@@ -50,6 +50,49 @@ function lightMode(){
 }
 
 /*~~~~~~~~~~~~~~~ TABS ~~~~~~~~~~~~~~~*/
+let tabs = document.querySelectorAll('.tab');
+let indicator = document.querySelector(".indicator");
+const all=document.querySelectorAll('.work_card');
+const ui=document.querySelectorAll('.uiux');
+const brand=document.querySelectorAll('.branding');
+const app=document.querySelectorAll('.apps');
+
+indicator.style.width = tabs[0].getBoundingClientRect().width + "px"; 
+indicator.style.left = tabs[0].getBoundingClientRect().left - tabs[0].parentElement.getBoundingClientRect().left + "px";
+
+tabs.forEach((tab) => {
+tab.addEventListener("click", () => {
+indicator.style.width = tab.getBoundingClientRect().width + "px";
+ indicator.style.left = tab.getBoundingClientRect().left-tab.parentElement.getBoundingClientRect().left +"px";
+tabs.forEach(t => t.classList.remove("text-whiteColor")); 
+tab.classList.add("text-whiteColor");
+
+const tabval=tab.getAttribute('data-tabs');
+
+all.forEach(item=>{
+    item.style.display="none"
+});
+
+if(tabval=="uiux"){
+    ui.forEach(item=>{
+        item.style.display="block"
+    });
+}else if(tabval=="branding"){
+    brand.forEach(item=>{
+        item.style.display="block"
+    });
+}else if(tabval=="apps"){
+    app.forEach(item=>{ 
+        item.style.display="block"
+    });
+}else{
+    all.forEach(item=>{
+        item.style.display="block"
+    });
+}
+
+})
+})
 
 /*~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~*/
 
